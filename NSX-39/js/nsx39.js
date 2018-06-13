bbb5c038b776c59789447a2adf31181bad83c7ef
Unknown <NicholasR@hotmail.de>
2018-06-13 02:57:43 +0200

    changed nsx39.js

--- NSX-39/js/nsx39.js
+++ NSX-39/js/nsx39.js
@@ -8,52 +8,49 @@ var Nsx39=function(){
     this.sysExSuffix=[ 0xF7 ];
     this.devide=64;
     this.textMap={
-        1: {
-            "あ": 0x00, "い": 0x01, "う": 0x02, "え": 0x03, "お": 0x04, "か": 0x05, "き": 0x06, "く": 0x07, 
-            "け": 0x08, "こ": 0x09, "が": 0x0A, "ぎ": 0x0B, "ぐ": 0x0C, "げ": 0x0D, "ご": 0x0E,
-            "さ": 0x15, "す": 0x17, 
-            "せ": 0x18, "そ": 0x19, "ざ": 0x1A, "ず": 0x1C, "ぜ": 0x1D, "ぞ": 0x1E,  
-            "づ": 0x1C, 
-            "し": 0x20, "じ": 0x25, 
-            "た": 0x29, "て": 0x2C, "と": 0x2D, "だ": 0x2E,  
-            "で": 0x31, "ど": 0x32, "ち": 0x36, 
-            "つ": 0x3C, "な": 0x3F, 
-            "に": 0x40, "ぬ": 0x41, "ね": 0x42, "の": 0x43, "は": 0x47, 
-            "ひ": 0x48, "ふ": 0x49, "へ": 0x4A, "ほ": 0x4B, "ば": 0x4C, "び": 0x4D, "ぶ": 0x4E, "べ": 0x4F, 
-            "ぼ": 0x50, "ぱ": 0x51, "ぴ": 0x52, "ぷ": 0x53, "ぺ": 0x54, "ぽ": 0x55,  
-            "ま": 0x64, "み": 0x65, "む": 0x66, "め": 0x67, 
-            "も": 0x68, "や": 0x6C, "ゆ": 0x6D, "よ": 0x6E, "ら": 0x6F, 
-            "り": 0x70, "る": 0x71, "れ": 0x72, "ろ": 0x73, "わ": 0x77, 
-            "ん": 0x7B, //, "ん": 0x7C, "ん": 0x7D, "ん": 0x7E, "ん": 0x7F
-            "を": 0x7A
-        },
-        2: {
-            "きゃ": 0x0F,
-            "きゅ": 0x10, "きょ": 0x11, "ぎゃ": 0x12, "ぎゅ": 0x13, "ぎょ": 0x14,  
-            "すぃ": 0x16,
-            "ずぃ": 0x1B,
-            "づぇ": 0x1D, "づぉ": 0x1E, "しゃ": 0x1F,
-            "づぁ": 0x1A, "づぃ": 0x1B,
-            "しゅ": 0x21, "しぇ": 0x22, "しょ": 0x23, "じゃ": 0x24,
-            "じゅ": 0x26, "じぇ": 0x27,
-            "じょ": 0x28, "てぃ": 0x2A, "とぅ": 0x2B,
-            "でぃ": 0x2F,
-            "どぅ": 0x30,             
-            "てゅ": 0x33, "でゅ": 0x34, "ちゃ": 0x35,
-            "ちゅ": 0x37,
-            "ちぇ": 0x38, "ちょ": 0x39, "つぁ": 0x3A, "つぃ": 0x3B,
-            "つぇ": 0x3D, "つぉ": 0x3E, 
-            "にゃ": 0x44, "にゅ": 0x45, "にょ": 0x46,
-            "ひゃ": 0x56, "ひゅ": 0x57,
-            "ひょ": 0x58, "びゃ": 0x59, "びゅ": 0x5A, "びょ": 0x5B, "ぴゃ": 0x5C, "ぴゅ": 0x5D, "ぴょ": 0x5E, "ふぁ": 0x5F, 
-            "ふぃ": 0x60, "ふゅ": 0x61, "ふぇ": 0x62, "ふぉ": 0x63,
-            "みゃ": 0x69, "みゅ": 0x6A, "みょ": 0x6B, 
-            "りゃ": 0x74, "りゅ": 0x75, "りょ": 0x76,
-            "うぃ": 0x78, "うぇ": 0x79, "うぉ": 0x7A
-        }
+		1: {
+            "a": 0x00, "i": 0x01, "u": 0x02, "e": 0x03, "o": 0x04,
+			"n": 0x7B
+		},
+		2: {
+			"ka": 0x05, "ki": 0x06, "ku": 0x07, "ke": 0x08, "ko": 0x09,
+			"ga": 0x0A, "gi": 0x0B, "gu": 0x0C, "ge": 0x0D, "go": 0x0E,
+            "sa": 0x15, "si": 0x16, "su": 0x17, "se": 0x18, "so": 0x19,
+			"za": 0x1A, "zi": 0x1B, "zu": 0x1C, "ze": 0x1D, "zo": 0x1E,
+			"ja": 0x24, "ji": 0x25, "ju": 0x26, "je": 0x27, "jo": 0x28, 
+            "ta": 0x29, "ti": 0x2A, "tu": 0x2B, "te": 0x2C, "to": 0x2D,
+			"da": 0x2E, "di": 0x2F, "du": 0x30, "de": 0x31, "do": 0x32,
+			"na": 0x3F, "ni": 0x40, "nu": 0x41, "ne": 0x42, "no": 0x43,
+			"ha": 0x47, "hi": 0x48, "fu": 0x49, "he": 0x4A, "ho": 0x4B,
+			"ba": 0x4C, "bi": 0x4D, "bu": 0x4E, "be": 0x4F, "bo": 0x50,
+			"pa": 0x51, "pi": 0x52, "pu": 0x53, "pe": 0x54, "po": 0x55,
+			"fa": 0x5F, "fi": 0x60, "fe": 0x62, "fo": 0x63, 
+            "ma": 0x64, "mi": 0x65, "mu": 0x66, "me": 0x67, "mo": 0x68,
+			"ya": 0x6C, "yu": 0x6D, "yo": 0x6E,
+			"ra": 0x6F, "ri": 0x70, "ru": 0x71, "re": 0x72, "ro": 0x73,
+			"wa": 0x77, "wi": 0x78, "we": 0x79, "wo": 0x7A,
+			"n1": 0x7B, "n2": 0x7C, "n3": 0x7D, "n4": 0x7E, "n5": 0x7F
+		},
+		3: {
+            "kya": 0x0F, "kyu": 0x10, "kyo": 0x11,
+			"gya": 0x12, "gyu": 0x13, "gyo": 0x14,  
+            "swi": 0x16,
+            "zwa": 0x1A, "zwi": 0x1B, "zwe": 0x1D, "zwo": 0x1E, 
+            "sha": 0x1F, "shi": 0x20, "shu": 0x21, "she": 0x22, "sho": 0x23,   
+            "tyu": 0x33, "dyu": 0x34,
+			"cha": 0x35, "chi": 0x36, "chu": 0x37, "che": 0x38, "cho": 0x39,
+			"tsa": 0x3A, "tsi": 0x3B, "tsu": 0x3C, "tse": 0x3D, "tso": 0x3E, 
+            "nya": 0x44, "nyu": 0x45, "nyo": 0x46,
+            "hya": 0x56, "hyu": 0x57, "hyo": 0x58,
+			"bya": 0x59, "byu": 0x5A, "byo": 0x5B,
+			"pya": 0x5C, "pyu": 0x5D, "pyo": 0x5E, 
+            "fyu": 0x61, 
+            "mya": 0x69, "myu": 0x6A, "myo": 0x6B, 
+            "rya": 0x74, "ryu": 0x75, "ryo": 0x76
+		}
     };
     // create Phonetic Symbol's RegExp 
-    for(var i=1, rexp=[]; i<=2; i++) {
+    for(var i=1, rexp=[]; i<=3; i++) {
         for(var key in this.textMap[i]) {
             rexp.push(key);
         }
@@ -79,12 +76,12 @@ Nsx39.prototype={
         outTmp=[];
         outTmp.push(Idx);
         
-        var tmp=ls;
-        if(chop==true) {
-            tmp=ls.substr(0, devide);
-        }
+        var tmp=ls.replace(/ /g,"");
 
-        if(ls.substr(devide-1, 2).length==2 && typeof this.textMap[2][ls.substr(devide-1, 2)]!="undefined") {
+		if(ls.substr(devide-2, 3).length==3 && typeof this.textMap[3][ls.substr(devide-2, 3)]!="undefined") {
+            tmp=ls.substr(0, devide+2);
+        }
+        else if(ls.substr(devide-1, 2).length==2 && typeof this.textMap[2][ls.substr(devide-1, 2)]!="undefined") {
             tmp=ls.substr(0, devide+1);
         }
         
@@ -93,17 +90,29 @@ Nsx39.prototype={
         
         for(var j=0; j<tmp.length; j++) {
             var t;
-            t=this.textMap[2][tmp.substring(j, j+2)];
-            if(typeof t==="undefined") {
-                t=this.textMap[1][tmp.substring(j, j+1)];
-            } else {
+			
+			t=this.textMap[3][tmp.substring(j, j+3)];
+			if(typeof t==="undefined") {
+				t=this.textMap[2][tmp.substring(j, j+2)];
+				if(typeof t==="undefined") {
+					t=this.textMap[1][tmp.substring(j, j+1)];
+				} else {
+					j++;
+				}
+			} else {
                 j++;
+				j++;
             }
+
             if(typeof t==="undefined") {
                 errCount++;
             } else {
                 outTmp.push(t);
             }
+			
+			if(chop==true && outTmp.length > 64) {
+				break;
+			}
         }
         out=this.sysExPrefix.concat(outTmp, this.sysExSuffix);
 
@@ -126,7 +135,7 @@ Nsx39.prototype={
             for(var val0 in this.textMap ) {
                 for(var val1 in this.textMap[val0]) {
                     if(this.textMap[val0][val1]==msg[i]) {
-                        out = out + val1;
+                        out = out + val1 + " ";
                         breakF=true;
                         break;
                     }
@@ -143,10 +152,16 @@ Nsx39.prototype={
     },
     createLyricsReqOrder: function(w){
         for(var i=0, arOut=[]; i<w.length; i++) {
-            arOut.push(i);
-            if(w.substr(i+1, 1).match(/ぁ|ぃ|ぅ|ぇ|ぉ|ゃ|ゅ|ょ|っ/)!=null) {
-                i++;
-            }
+			if(!w.substr(i, 1).includes(" ")){
+				arOut.push(i);
+			}
+			if(w.substr(i, 3).length==3 && typeof this.textMap[3][w.substr(i, 3)]!="undefined") {
+				i++;
+				i++;
+			}
+			else if(w.substr(i, 2).length==2 && typeof this.textMap[2][w.substr(i, 2)]!="undefined") {
+				i++;
+			}
         }
         return arOut;
     },
